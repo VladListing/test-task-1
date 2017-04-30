35531fd6930996e070e55736de6f7eb19f642f39
VladListing <chvch@mail.ru>
2017-04-30 16:59:28 +0700

    30-04-2017 region

--- test-task-1/test-task-1/Program.cs
+++ test-task-1/test-task-1/Program.cs
@@ -10,10 +10,11 @@ using System.Threading.Tasks;
 
 namespace test_task_1
 {
-    
 
+    //====================================================== обьявляем структуры ================ start  ==================================|
+    #region описание структура header
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct Header //обьявление структуры "Header"
+    public struct Header //обьявление структуры "Header "
     {
         public int version;
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
@@ -28,7 +29,9 @@ namespace test_task_1
         }
 
     }
+    #endregion
 
+    #region описание структура trade
     [Serializable]
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct TradeRecord //обьявление структуры "TradeRecord"
@@ -51,7 +54,8 @@ namespace test_task_1
 
         }
     }
-
+    #endregion
+    //====================================================== обьявляем структуры ================ end  =================================|
 
 
 
@@ -59,89 +63,94 @@ namespace test_task_1
     {
         public const
         string path = @"D:\\_LISTING_\B-files\StructTrade_Line_1200.dat";  //путь и имя бинарного файла со структурами
-        static void Main(string[] args)
+        static void Main(string[] args) // тестовое задание 1:
         {
-            // тестовое задание 1:
 
-            BinaryFormatter formatter = new BinaryFormatter();
+
+            //BinaryFormatter formatter = new BinaryFormatter();
 
 
 
             // подзадача 1.конвертация бинарных файлов в формат " .*CSV "========================================================================================================================
 
-            //int schetchik = 0;//счетчик количеста проходов цикла
+            int schetchik = 0;//счетчик количеста проходов цикла
             Header[] header = new Header[1]; // создание экземпяра структуры "TradeRecord" на 1-ну строку
+            TradeRecord[] trade = new TradeRecord[110]; // создание экземпяра структуры "TradeRecord" на Х строк
             TradeRecord[] newtrade = new TradeRecord[10000]; // создание экземпяра структуры "TradeRecord" на X строк
-            //TradeRecord[] trade = new TradeRecord[1000]; // создание экземпяра структуры "TradeRecord" на Х строк
 
-            try
-          {
-                // создаем объект BinaryReader (поток чтение  из бинарного файла)
-                //using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
-                //{
-
-                //     reader.BaseStream.Position = 0;// устанавливаем "курсор" на 0-вую позицию в читаемом бинарном файле
-
-                //     // считываем через цикл каждое значение полей строк структуры "Header" из файла "treding.dat" и выводим на экран , а так же через конструктор заполняем поля структуры
-                //     while (reader.BaseStream.Position < 12)//пока позиция курсора не превышает 12-тую в бинарном файле
-                //     {
-                //         //var poz = reader.Current;
-                //         int version = reader.ReadInt32();
-                //         string type = reader.ReadString();
-
-                //         Console.WriteLine("версия: {0}      тип: {1} ", version, type);
-
-                //         header[0] = new Header(version, type);// инециализируем поля , присваиваем им значения через конструктор
-                //         schetchik = schetchik + 1;
-                //     }
-
-                //     Console.WriteLine("Счетчик вычитанных из бинарного файла строк структуры header: {0}  ", schetchik);
-                //     schetchik = 0;
-
-                //     Console.WriteLine();//пустая строка
-
-                //     reader.BaseStream.Position = 12;// устанавливаем "курсор" на 12-вую позицию в бинарном файле
-
-                //     // пока не достигнут конец файла
-                //     // считываем через цикл каждое значение полей строк структуры "TradeRecord" из файла "treding.dat" и выводим на экран
-                //     while (reader.PeekChar() > -1)// пока не достигнут конец файла
-                //     {
-                //         int id = reader.ReadInt32();
-                //         int account = reader.ReadInt32();
-                //         double volume = reader.ReadDouble();
-                //         string comment = reader.ReadString();
-
-                //         //Console.WriteLine("id: {0}      счет: {1}     уровень: {2}       комментарий: {3} ", id, account, volume, comment);
-
-                //         trade[schetchik] = new TradeRecord(id, account, volume, comment );// инециализируем поля структуры, присваиваем им значения через конструктор
-                //         schetchik = schetchik + 1;
-
-
-
-                //     }
-
-                //     Console.WriteLine("Счетчик вычитанных из бинарного файла строк структуры trade: {0}  ", schetchik);
-                //     schetchik = 0;
-                // }
-
-                using (FileStream f = new FileStream(path, FileMode.OpenOrCreate))
+            try//'''''''''  try '''''''' оператор выполнение которого может привести к ошибке ''''''''''''начало ''''|
+            #region
+            {
+                //================== создаем объект BinaryReader (чтение  из бинарного файла)=================================start=========================|
+                #region BinaryReader
+                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
                 {
-                    newtrade = (TradeRecord[])formatter.Deserialize(f);
+                    reader.BaseStream.Position = 0;// устанавливаем "курсор" на 0-вую позицию в читаемом бинарном файле
 
-                    //Console.WriteLine("Объект десериализован");
-                    // Console.WriteLine("Имя: {0} --- Возраст: {1}", newPerson.Name, newPerson.Age);
-                    Console.WriteLine("Объект десериализован");
-                    foreach (TradeRecord z in newtrade)
+                    // считываем через цикл каждое значение полей строк структуры "Header" из файла "treding.dat" и выводим на экран , а так же через конструктор заполняем поля структуры
+                    #region  Read header
+                    while (reader.BaseStream.Position < 12)//пока позиция курсора не превышает 12-тую в бинарном файле
                     {
-                        //Console.WriteLine("id: {0}      счет: {1}     уровень: {2}       комментарий: {3} ", z.id, z.account, z.volume, z.comment);
-                    }
-                }
+                        //var poz = reader.Current;
+                        int version = reader.ReadInt32();
+                        string type = reader.ReadString();
 
+                        Console.WriteLine("версия: {0}      тип: {1} ", version, type);
+
+                        header[0] = new Header(version, type);// инециализируем поля , присваиваем им значения через конструктор
+                        schetchik = schetchik + 1;
+                    }
+
+                    Console.WriteLine("Счетчик вычитанных из бинарного файла строк структуры header: {0}  ", schetchik);
+                    schetchik = 0;
+                    #endregion
+                    Console.WriteLine();//пустая строка
+
+                    #region Read trade
+                    reader.BaseStream.Position = 12;// устанавливаем "курсор" на 12-вую позицию в бинарном файле
+
+                    // пока не достигнут конец файла
+                    // считываем через цикл каждое значение полей строк структуры "TradeRecord" из файла "treding.dat" и выводим на экран
+                    while (reader.PeekChar() > -1)// пока не достигнут конец файла
+                    {
+                        int id = reader.ReadInt32();
+                        int account = reader.ReadInt32();
+                        double volume = reader.ReadDouble();
+                        string comment = reader.ReadString();
+
+                        //Console.WriteLine("id: {0}      счет: {1}     уровень: {2}       комментарий: {3} ", id, account, volume, comment);
+
+                        trade[schetchik] = new TradeRecord(id, account, volume, comment);// инециализируем поля структуры, присваиваем им значения через конструктор
+                        schetchik = schetchik + 1;
+                    }
+
+                    Console.WriteLine("Счетчик вычитанных из бинарного файла строк структуры trade: {0}  ", schetchik);
+                    schetchik = 0;
+                    #endregion
+
+                }
+                #endregion
+                //================== создаем объект BinaryReader (чтение  из бинарного файла)=================================end=====================|
+
+
+                #region Deserialize
+                //using (FileStream f = new FileStream(path, FileMode.OpenOrCreate))
+                //{
+                //    newtrade = (TradeRecord[])formatter.Deserialize(f);
+                //    //Console.WriteLine("Объект десериализован");
+                //    // Console.WriteLine("Имя: {0} --- Возраст: {1}", newPerson.Name, newPerson.Age);
+                //    Console.WriteLine("Объект десериализован");
+                //    foreach (TradeRecord z in newtrade)
+                //    {
+                //        //Console.WriteLine("id: {0}      счет: {1}     уровень: {2}       комментарий: {3} ", z.id, z.account, z.volume, z.comment);
+                //    }
+                //}
+                #endregion
 
 
                 //-----------------------------  через цыклы выгружаем значения полей структур в файл  *.CSV  -------------------------------
-
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\\_LISTING_\B-files\StructTrade_Line_1200.CSV")) 
+                #region unloading in .*CSV file
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\\_LISTING_\B-files\StructTrade_Line_1200.CSV"))
                 {
 
                     //    foreach (Header t in header)
@@ -155,7 +164,7 @@ namespace test_task_1
                     //    }
 
 
-                    foreach (TradeRecord t in newtrade )
+                    foreach (TradeRecord t in newtrade)
                     {
                         file.Write(t.id);
                         file.Write(";");
@@ -171,12 +180,17 @@ namespace test_task_1
                     }
 
                 }
-                //-----------------------------  через цыклы выгружаем значения полей структур в файл  *.CSV  -------------------------------
+                #endregion
+                //---------------------------------------------------------------------------------------------------------------------------
 
-            }
-             //======================================   конец подзадачи 1  ================================================================================================================
+                #endregion
+            }//'''''''''  try '''''''' оператор выполнение которого может привести к ошибке ''''''''''''конец ''''|
 
-            
+
+
+            //======================================   конец подзадачи 1  ================================================================================================================
+
+
 
 
 
@@ -196,9 +210,9 @@ namespace test_task_1
 
             //=====================================================================================================================================================
 
-            
-            
-            
+
+
+
             // вывод сообщения о возникшем исключении
             catch (Exception m)
             {
