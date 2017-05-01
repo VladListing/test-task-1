a5cb554a4b419f97d6979ae23a2e22afb5890fff
VladListing <chvch@mail.ru>
2017-05-01 19:21:58 +0700

    01-05-2017 реализоано преобразование на основе классов

--- test-task-1/test-task-1/Program.cs
+++ test-task-1/test-task-1/Program.cs
@@ -8,122 +8,33 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test_task_1
+namespace test_task_1  //конвертация бинарных файлов в формат " .*CSV 
 {
-
-    //====================================================== обьявляем структуры ================ start  ==================================|
-       #region описание структура trade
-    
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct TradeRecord //обьявление структуры "TradeRecord"
-    {
-
-        public int id;
-        public int account;
-        public double volume;
-        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
-        public string comment;
-
-
-        //конструктор "TradeRecord"
-        public TradeRecord(int a, int b, double c, string d)
-        {
-            id = a;
-            account = b;
-            volume = c;
-            comment = d;
-
-        }
-    }
-    #endregion
-    //====================================================== обьявляем структуры ================ end  =================================|
-
-
 
     class Program
     {
-        public const
-        string path = @"D:\\_LISTING_\B-files\StructTrade_Line_110.dat";  //путь и имя бинарного файла со структурами
+        public const string path_dat = @"D:\\Trade.dat";  //путь и имя бинарного файла со структурами
+        public const string path_CSV = @"D:\\Trade.CSV";  //путь и имя  создаваемого файла с разделителями, типа *.CSV
+
         static void Main(string[] args) // тестовое задание 1:
         {
-            
-            // подзадача 1.конвертация бинарных файлов в формат " .*CSV "========================================================================================================================
 
-            int schetchik = 0;//счетчик количеста проходов цикла
+            //секция критичная в части исключений
+            try
 
-            //======================= создаем экземпляры (обьекты) структур и инициализируем их поля ============= start ==============================|
-            #region creation struct trade
-            TradeRecord[] trade = new TradeRecord[110]; // создание экземпяра структуры "TradeRecord" на Х строк
-            #endregion
-            //============= создаем экземпляры (обьекты) структур и инициализируем их поля ======================= end ==========================|
-
-            try//'''''''''  try '''''''' оператор выполнение которого может привести к ошибке ''''''''''''начало ''''|
-            #region
             {
-                //================== создаем объект BinaryReader (чтение  из бинарного файла)=================================start=========================|
-                #region BinaryReader loading struct trade from *.dat file
-                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
-                {
-                    #region Read trade
-
-                    reader.BaseStream.Position = 0;// устанавливаем "курсор" на 0-вую позицию в читаемом бинарном файле
-                    // пока не достигнут конец файла
-                    // считываем через цикл каждое значение полей строк структуры "TradeRecord" из файла "treding.dat" и выводим на экран
-                    while (reader.PeekChar() > -1)// пока не достигнут конец файла
-                    {
-                        int id = reader.ReadInt32();
-                        int account = reader.ReadInt32();
-                        double volume = reader.ReadDouble();
-                        string comment = reader.ReadString();
-
-                        //Console.WriteLine("id: {0}      счет: {1}     уровень: {2}       комментарий: {3} ", id, account, volume, comment);
-
-                        trade[schetchik] = new TradeRecord(id, account, volume, comment);// инециализируем поля структуры, присваиваем им значения через конструктор
-                        schetchik = schetchik + 1;
-                    }
-
-                    Console.WriteLine("Счетчик вычитанных из бинарного файла строк структуры trade: {0}  ", schetchik);
-                    schetchik = 0;
-                    #endregion
-                }
-                #endregion
-                //================== создаем объект BinaryReader (чтение  из бинарного файла)=================================end=====================|
+                //класс 'ReaderFromBinaryFiles' вычитывает данные из бинарного файла и возвращает коллекцию структурированных данных  
+                ReaderFromBinaryFiles readerFromBinaryFiles = new ReaderFromBinaryFiles(path_dat);
+                List<TradeRecord> Collektion = readerFromBinaryFiles.fromBinaryFile(path_dat);
 
 
-                //-----------------------------  через цыклы выгружаем значения полей структур в файл  *.CSV  -------------------------------
-                #region unloading struct trade in *.CSV file
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\\_LISTING_\B-files\StructTrade_Line_110.CSV"))
-                {
+                //класс 'ReadToCSV' получает коллекцию структурированных данных и генерирует из нее конечный файл *.CSV 
+                ReadToCSV readToCSV = new ReadToCSV(Collektion, path_CSV);//создаем экземпляр классауктур
+                readToCSV.toCSV(Collektion, path_CSV);//вызов метода класса 
 
-                    foreach (TradeRecord t in trade)
-                    {
-                        file.Write(t.id);
-                        file.Write(";");
-                        file.Write(t.account);
-                        file.Write(";");
-                        file.Write(t.volume);
-                        file.Write(";");
-                        file.Write(t.comment);
+            }
 
-                        file.WriteLine(";");
-
-                        schetchik = schetchik + 1;
-                        //Console.WriteLine("id: {0}      счет: {1}     уровень: {2}       комментарий: {3} ", t.id, t.account, t.volume, t.comment);
-                    }
-
-                    Console.WriteLine("Счетчик строк структуры trade конвертированных из бинарного формата в фрмат CSV : {0}  ", schetchik);
-                    schetchik = 0;
-
-                }
-                #endregion
-                //---------------------------------------------------------------------------------------------------------------------------
-
-                #endregion
-            }//'''''''''  try '''''''' оператор выполнение которого может привести к ошибке ''''''''''''конец ''''|
-
-            //======================================   конец подзадачи 1  ================================================================================================================
-
-            //-----вывод сообщения о возникшем исключении---------
+            //сообщения о возникшем исключении
             #region исключения
             catch (Exception m)
             {
@@ -131,7 +42,7 @@ namespace test_task_1
             }
             Console.ReadLine();
             #endregion
-            //---------------------------------------------------
+
         }
     }
 }
---
+++ test-task-1/test-task-1/ReadToCSV.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_task_1
+{
+    class ReadToCSV
+    {
+
+
+    }
+}
